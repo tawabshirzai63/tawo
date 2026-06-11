cb36fd9f55e93946b61d350668f4ff517130a226
tawabshirzai63 <tawabshirzai63@users.noreply.github.com>
2026-06-11 15:30:53 +0000

    Build Missed Call Recovery and Automated Notification Engine

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,13 @@
 const googleCalendar = require('./googleCalendar');
 const twilio = require('./twilio');
 const ai = require('./ai');
+const mail = require('./mail');
+const notificationEngine = require('./notificationEngine');
 
 module.exports = {
     googleCalendar,
     twilio,
-    ai
+    ai,
+    mail,
+    notificationEngine
 };
